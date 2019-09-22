9314383b093d098eee4db459f53e18379f7689e2
seerosecode <keshalakemusic@gmail.com>
2019-09-22 11:44:08 -0400

    rendered blogs using medium-pixelpoint widget. rearranged table

--- src/components/projects.js
+++ src/components/projects.js
@@ -5,7 +5,7 @@ class Projects extends Component {
   render(){
     return(
       <div className="projects">
-        <iframe width="600" height="402" src="https://www.youtube.com/embed/44erbzteLdI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="600" height="402" src="https://www.youtube.com/embed/44erbzteLdI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen className="video"></iframe>
       </div>
     )
   }
